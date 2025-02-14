bdea9c7373f58f857c746bfb68a772c98e228770
julien.pivi <julien.pivi@olea-medical.com>
2025-02-14 09:36:46 +0100

    rendering

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+    //BrowserWIndow is the page wrapper.
+    ///Electron follow JavaScript naming convention in PascalCase not in camelCase.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+          }
+    })
 
-  win.loadFile('index.html')
+    win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
+    //Open a window if none are open (macOS)
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+
 })
+
+//Quit the app when all windows are closed (Windows & Linux)
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
